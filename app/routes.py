d565f2cf6f514036e530bd2cfbd2a6268b2d19e2
Sydney Alberson <sydney.alberson@gmail.com>
2023-04-21 11:06:58 -0400

    Created routes file and made the handle_crystals function

---
+++ app/routes.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, jsonify
+
+class Crystal():
+
+    def __init__(self, id, name, color, powers):
+        self.id = id
+        self.name = name
+        self.color = color
+        self.powers = powersç
+
+# create a list of crystals
+crystals = [
+    Crystal(1, "Amethyst", "Purple", ["Infinite knowledge and wisdom"]),
+    Crystal(2, "Tiger's Eye", "Gold", ["Confidence", "Strength"]),
+    Crystal(3, "Rose Quartz", "Pink", ["Love"])
+]
+
+crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
+
+@crystal_bp.route("", methods=["GET"])
+
+def handle_crystal():
+    crystal_response = []
+
+    for crystal in crystals:
+        crystal_response.append({
+            "id": crystal.id,
+            "name": crystal.name,
+            "color": crystal.color,
+            "powers": crystal.powers
+        })
+
+    return jsonify(crystal_response)
